c1ae40b4bc51071ff2993f1647e296ea2eea2a93
Gabriel Lopez <gabriellopez4195@gmail.com>
2026-05-04 20:38:17 -0300

    Ejercicio 3 completado

---
+++ instituto.js
@@ -0,0 +1,46 @@
+let colors = require(`colors`)
+let alumno = {
+    nombre: `Gabriel`,
+    edad: 21,
+    inscriptoAMaterias: [`lengua`, `matematica`],
+    debeCorrelativa: false
+}
+function validarCorrelativa(alumno) {
+    console.log(`----Validando correlativas----`.yellow)
+    return new Promise((resolve, reject) => {
+        setTimeout(()=>{
+            if (!alumno.debeCorrelativa) {
+                resolve(`Puede inscribirse`.green)
+            }
+            else {
+                reject(`Debe correlativa`.red)
+            }
+        },2000)
+    })
+}
+function inscripcionMateria(alumno, materia) {
+    console.log(`----Inscribiendo a materia----`.yellow)
+    return new Promise((resolve, reject) => {
+        setTimeout(()=>{
+            alumno.inscriptoAMaterias.push(materia)
+            resolve(`Inscripcion a ${materia} completada`.green)
+        },5000)
+    })
+}
+
+validarCorrelativa(alumno)
+    .then(response => {
+        console.log(response)
+        return inscripcionMateria(alumno, `ingles`)
+    })
+    .then(response => {
+        console.log(`----INSCRIPCION EXITOSA----`.green)
+        console.log(response)
+    })
+    .catch(error => {
+        console.log(`----NO PUEDE INSCRIBIRSE----`.red)
+        console.log(error)
+    })
+    .finally(end=>{
+        console.log(`----FINALIZO LA OPERACION----`.yellow)
+    })
---
+++ mercadoRestringido.js
@@ -0,0 +1,90 @@
+let colors = require(`colors`)
+
+let prod1 = {
+    id:1,
+    nombreProducto:`lapiz`,
+    precio:10,
+    stock:10
+}
+let prod2 = {
+    id:2,
+    nombreProducto:`hoja`,
+    precio:5,
+    stock:20
+}
+let prod3 = {
+    id:3,
+    nombreProducto:`lapicera`,
+    precio:15,
+    stock:20
+}
+let prod4 = {
+    id:4,
+    nombreProducto:`goma`,
+    precio:5,
+    stock:30
+}
+let prod5 = {
+    id:5,
+    nombreProducto:`sacapuntas`,
+    precio:5,
+    stock:30
+}
+
+let productos=[prod1,prod2,prod3,prod4,prod5]
+
+function validarStock(busquedaProducto, productos) {
+    console.log(`----Validando stock----`.yellow)
+    return new Promise((resolve, reject) => {
+        let producto = productos.find((element)=>{
+            return element.nombreProducto===busquedaProducto
+        })
+        setTimeout(() => {
+            if (producto!=undefined) {
+                console.log(`producto encontrado`.yellow)
+                resolve(producto)
+            } else {
+                reject(`producto no encontrado`.red)
+            }
+        }, 2000)
+    })
+}
+function realizarVenta(producto) {
+    console.log(`Realizando venta`.yellow)
+
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            producto.stock = producto.stock - 1
+            resolve(`Venta realizada - Stock actualizado de producto ${producto.nombreProducto}: ${producto.stock}`.green)
+        }, 1000)
+    })
+}
+function generarEtiqueta(nombreProducto) {
+    console.log(`Generando etiqueta`.yellow)
+
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve(`Etiqueta generada de ${nombreProducto}`.green)
+        },4000)
+    })
+}
+
+productoAComprar=`hoja`
+validarStock(productoAComprar,productos)
+.then(response=>{
+    productoAComprar=response
+    return realizarVenta(response)
+})
+.then(response=>{
+    console.log(response)
+    return generarEtiqueta(productoAComprar.nombreProducto)
+})
+.then(response=>{
+    console.log(response)
+})
+.catch(error=>{
+    console.log(error)
+})
+.finally(end=>{
+    console.log(`----FINALIZO LA OPERACION----`.yellow)
+})
